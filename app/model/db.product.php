948193e0ee1b1dec57806c7f6f8ca6365ff10927
MarionDuFaouet <lozachm@gmail.com>
2024-03-22 18:31:20 +0100

    maj

--- app/model/db.product.php
+++ app/model/db.product.php
@@ -4,12 +4,11 @@ include_once "db.connec.php";
 
 // get all from product table
 //pour afficher les produits dans ma vue/produit
-//affiche le nom, le prix, la description, l'image
 function getProducts() {
     $result = array();
     try {
         $cnx = connexionPDO();
-        $query = $cnx->prepare("SELECT name, designation, unitPrice, pictureLink FROM Product");
+        $query = $cnx->prepare("SELECT name, designation, unitPrice, pictureRef FROM Product");
         $query->execute();
         $row = $query->fetch(PDO::FETCH_ASSOC);
         while ($row) {
@@ -29,7 +28,7 @@ function getProducts() {
 function getProductById($productId) {
     try {
         $cnx = connexionPDO();
-        $query = $cnx->prepare("SELECT name, designation, unitPrice, pictureLink FROM Product WHERE productId=:productId");
+        $query = $cnx->prepare("SELECT name, designation, unitPrice, pictureRef FROM Product WHERE productId=:productId");
         $query->bindValue(':productId', $productId, PDO::PARAM_INT);
         $query->execute();
         $result = $query->fetch(PDO::FETCH_ASSOC);
@@ -41,14 +40,14 @@ function getProductById($productId) {
 
 // add products
 // va enregistrer un nouveau produit depuis adminProduct
-function createProduct($name, $designation, $unitPrice, $pictureLink){
+function createProduct($name, $designation, $unitPrice, $pictureRef){
     try{
         $cnx = connexionPDO();
-        $query = $cnx ->prepare("INSERT into Product (name, designation, unitPrice, pictureLink) values(:name,:designation, :unitPrice, :pictureLink)");
+        $query = $cnx ->prepare("INSERT into Product (name, designation, unitPrice, pictureRef) values(:name,:designation, :unitPrice, :pictureRef)");
         $query->bindValue(':name,', $name, PDO::PARAM_STR);
         $query->bindValue(':designation,', $designation, PDO::PARAM_STR);
         $query->bindValue(':unitPrice,', $unitPrice, PDO::PARAM_STR); // ??? dans ma bdd, le type est en decimal!!!
-        $query->bindValue(':pictureLink,', $pictureLink, PDO::PARAM_STR);
+        $query->bindValue(':pictureRef,', $pictureRef, PDO::PARAM_STR);
         $result = $query->execute();
     } catch (PDOException $e) {
         die( "Erreur !: " . $e->getMessage() );
