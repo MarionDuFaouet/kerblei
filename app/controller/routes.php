b5270d6cf602c3a8213f50cac8fa946de1360c2a
MarionDuFaouet <lozachm@gmail.com>
2024-03-22 21:30:52 +0100

    correction routeur

--- app/controller/routes.php
+++ app/controller/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 function redirectTo($action="default") {
-	
+    
     $actions = array();
     $actions["default"] = "home.php";
     $actions["account"] = "account.php";
@@ -13,14 +13,16 @@ function redirectTo($action="default") {
     $actions["register"] = "register.php";
     $actions["legalNotices"] = "legalNotices.php";
 
-	$controler_id = $actions[$action];
+    $controller_id = $actions[$action];
 
-	//si le fichier n'existe pas :
-	if(! file_exists(__DIR__ . '/'. $controller_id) ) die("Le fichier : " . $controller_id . " n'existe pas !");
+    // Si le fichier n'existe pas :
+    if (!file_exists(__DIR__ . '/' . $controller_id)) {
+        die("Le fichier : " . $controller_id . " n'existe pas !");
+    }
 
-	//si la clé "action" existe dans notre tableau "lesActions" :
+    // Si la clé "action" existe dans notre tableau "lesActions" :
     if (array_key_exists($action, $actions)) {
-		// le fichier à inclure sera retourné :
+        // Le fichier à inclure sera retourné :
         return $controller_id;
     } else {
         return $actions["default"];
