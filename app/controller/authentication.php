ee5fdb9327cf76aa5d0bb633a57f8fb8edf525f8
MarionDuFaouet <lozachm@gmail.com>
2024-03-23 12:05:05 +0100

    modif lien db.authentication

--- app/controller/authentication.php
+++ app/controller/authentication.php
@@ -4,27 +4,27 @@ if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
     die('Erreur : '.basename(__FILE__));
 }
 
-require_once RACINE . "/model/authentication.inc.php";
+require_once RACINE . "/model/db.authentication.php";
 
 // recuperation des donnees GET, POST, et SESSION
 if (isset($_POST["mail"]) && isset($_POST["password"])){
     $mail=$_POST["mail"];
-    $pw=$_POST["password"];
+    $password=$_POST["password"];
 }
 else
 {
     $mail=null;
-    $pw=null;
+    $password=null;
 }
 
 // traitement si necessaire des donnees recuperees
 login($mail,$password);
 
 if (isLoggedOn()){ // si l'utilisateur est connecté on redirige vers le controleur monProfil
-    include RACINE . "/controller/viewCart.php";
+    include RACINE . "/app/controller/viewCart.php";
 }
 else{ 
-    include RACINE . "/views/viewRegister.php";
+    include RACINE . "app/views/viewRegister.php";
 }
 
 ?>
