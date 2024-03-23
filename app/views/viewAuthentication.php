61f2ec3b97832c32db1a59da20b59cfe1241e871
MarionDuFaouet <lozachm@gmail.com>
2024-03-23 11:51:51 +0100

    création formulaire de connexion

--- app/views/viewAuthentication.php
+++ app/views/viewAuthentication.php
@@ -1,18 +1,35 @@
 <?php
+
+// restreinte d'accès (definition sur entete.html.php)
 if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
+	// Un MVC utilise uniquement ses requêtes depuis le contrôleur principal : index.php
     die('Erreur : '.basename(__FILE__));
 }
 
-$title="Micro-Brasserie Kerblei - S'authentifier" ;
+
 include RACINE . "/views/header.php";
+// ou
+require_once RACINE. '/app/views/header.php';
+
 ?>
+
 <!-- question : puis-je ajouter ce head? (pour ne pas indexer cette page) -->
 <head>
     <meta name="robots" content="noindex" />
     <meta name="googlebot" content="noindex" />
 </head>
 
-<!-- formulaire de connexion -->
-<!-- lien vers création de compte -->
+<h1>Je Possède un compte</h1>
+<form action="./?action=authentication" method="POST">
+    <input type="text" name="mail" placeholder="Mon Email" /><br />
+    <input type="password" name="password" placeholder="Mon mot de passe"  /><br />
+    <input class="cta-button" type="submit" title="Connexion à votre compte" value="Je me connecte"/>
+</form>
+<br />
+<a href="./?action=register" class="cta-button" title="Cliquez pour créer un compte">Pas encore de compte ?</a>
 
+<hr>
+
+<?php require_once RACINE. '/app/views/footer.php'?>
+<!-- ou -->
 <?php include RACINE . "/views/footer.php";?>
