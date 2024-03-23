32358d39686e432b19f410b32a8eab57ec86312f
MarionDuFaouet <lozachm@gmail.com>
2024-03-23 09:09:11 +0100

    tentative malheureuse de réglage de chemins

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+// Un problème de chemin??? /\ pb linux?
+// si je mets un \ avant routes.php l.19, r disparait!!!
+
 // database connection file
 require RACINE . "/model/db.connec.php";
 
@@ -18,14 +21,23 @@ require RACINE . "/controller/routes.php";
 //authentication file
 require_once RACINE . "/model/authentication.php"; 
 
-// check for the action to perform
-if (isset($_GET["action"])) {
-	$action = $_GET["action"];
-}
+// // check for the action to perform
+// if (isset($_GET["action"])) {
+// 	$action = $_GET["action"];
+// }
 
-// Redirect to the corresponding action
+// // Redirect to the corresponding action
+// $file = redirectTo($action);
+// require RACINE . "/controller/" . $file;
+
+// Définir une valeur par défaut pour $action si elle n'est pas définie
+$action = isset($_GET["action"]) ? $_GET["action"] : "default";
+
+// Appeler la fonction redirectTo() pour obtenir le nom du fichier à inclure
 $file = redirectTo($action);
-require RACINE . "/controller/" . $file;
+
+// Inclure le fichier correspondant depuis le répertoire des contrôleurs
+require_once RACINE . "/controller/" . $file;
 
 ?>
 
