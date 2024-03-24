154cc6105449200fcb242ba42f066e79ef6dccf2
MarionDuFaouet <lozachm@gmail.com>
2024-03-24 14:38:30 +0100

    pb résolu

--- app/model/connec.php
+++ app/model/connec.php
@@ -1,9 +1,7 @@
 <?php
 
 function connexionPDO() {
-        $cnx = new PDO("mysql:host=".$_ENV['DB_HOST'].";dbname=".$_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASS']); 
-        // $cnx = new PDO("mysql:host=".$_ENV['DB_HOST'].";dbname=".$_ENV['DB_NAME'], $login, $password);        
-
+        $cnx = new PDO("mysql:host=".$_ENV['DB_HOST'].";dbname=".$_ENV['DB_NAME'], $_ENV['DB_LOGIN'], $_ENV['DB_PASSWORD']); 
         $cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $cnx;
 }
