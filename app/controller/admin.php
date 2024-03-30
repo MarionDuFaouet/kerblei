f101ac8bfa14d2343b6722d8e5aa992f072ddff1
MarionDuFaouet <lozachm@gmail.com>
2024-03-30 13:43:35 +0100

    backoffice c'est parti

--- app/controller/admin.php
+++ app/controller/admin.php
@@ -1,18 +1,1 @@
-<?php
-
-// is the (or a) user already logged ?
-if (!((isset($_SESSION['admin'])) && $_SESSION['admin'])) {
-    $_SESSION['msg']=['level'=> 'warning', 'content' => 'You must be an administrator to reach this page'];
-
-    /* It should not occurs if all restricted accesses are hidden or disable.
-       If it does, it means that routes are being violated.
-       Thus, forbidden routes must be closed as soon as possible : the router is probably the best place to do
-       that and redirection to the home page is appropriate.
-    */
-    
-    header("Location: ?action=home");   // access forbidden; immediatly redirected to home page
-    exit;                               // mandatory to abort the current script and update the $_SESSION 
-}
-
-else require "./views/admin.php";    // access allowed; go to the requested page
-?>
+<?php require_once RACINE . "./views/admin.php"; ?>
