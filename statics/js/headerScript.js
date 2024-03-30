fe75e2ee5a8b73959715daaad8f4f4124637c9e1
MarionDuFaouet <lozachm@gmail.com>
2024-03-30 21:00:22 +0100

    burger done

--- statics/js/headerScript.js
+++ statics/js/headerScript.js
@@ -1,8 +1,17 @@
-// burgerMenu
-const burgerMenu = document.querySelector(".burgerMenu")
-const navLinks = document.querySelector(".navLinks")
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.width = '250px'; // Définir la largeur du sidebar
 
-burgerMenu.addEventListener('click',()=>{
-navLinks.classList.toggle('mobilMenu')
-})
-// mais pourquoi ce script fonctionne si bizarrement?
+// Ajouter un gestionnaire d'événements aux liens de la sidebar pour fermer le sidebar lorsqu'un lien est cliqué
+const links = sidebar.querySelectorAll('a');
+links.forEach(link => {
+    link.addEventListener('click', hideSidebar);
+});
+
+
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.width = '0'; // Réduire la largeur du sidebar à 0 pour le masquer
+}
