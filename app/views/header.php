0da8aa6e5ed2dc55808ef09df6b865ee0d7ac4cc
MarionDuFaouet <lozachm@gmail.com>
2024-03-30 21:00:56 +0100

    burger done

--- app/views/header.php
+++ app/views/header.php
@@ -4,32 +4,35 @@
 <?php require RACINE . '/views/head.php' ?>
 
 <body>
-        <!-- burgerNav -->
-        <nav class="navbar">
-            <a href="./?action=default" class="logo">
-                <img src="./statics/images/zlogo1.png" alt="logo Brasserie Kerblei">
-            </a>
-            <div class="navLinks">
-                <ul>
-                    <li class="active"><a href="./?action=default">Accueil</a></li>
-                    <li><a href="./?action=products">Nos bières</a></li>
-            
-                    <!-- ---------------------------------------- -->
-                    <?php if (isset($_SESSION ['mail'])) { ?>
-                        <li><a href="./?action=cart"><i class="fa-solid fa-basket-shopping"></i> Panier / Nous trouver</a></li>
-                        <li><a href="./?action=account"><i class="fa-solid fa-user"></i></a></li>
-                    <?php } else { ?>
-                        <li><a href="./?action=authentication"><i class="fa-solid fa-basket-shopping"></i>Panier / Nous trouver</a></li>
-                        <li><a href="./?action=authentication"><i class="fa-solid fa-user"></i></a></li>
-                    <?php } ?>
-                    <!-- ?  --------------------------------------------- -->
-                </ul>
-            </div>
-            <!-- retirer l'image si je ne l'utilise pas -->
-            <!-- <img src="./statics/images/menu-btn.png" alt="menu déroulant" class="burgerMenu"> -->
-            <i class="fa-solid fa-bars burgerMenu"></i>
-        </nav>
-        <header></header>
+    <!-- burgerNav -->
+    <nav class="navbar">
+        <a href="./?action=default" class="logo"><img src="./statics/images/zlogo1.png" alt="logo Brasserie Kerblei"></a>
+        <ul class="sidebar">
+            <li onclick=hideSidebar()><i id="navCross" class="fa-solid fa-xmark"></i></li>
+            <li><a href="./?action=default">Accueil</a></li>
+            <li><a href="./?action=products">Nos bières</a></li>
+            <?php if (isset($_SESSION['mail'])) { ?>
+                <li><a href="./?action=cart"><i class="fa-solid fa-basket-shopping"></i> Panier / Nous trouver</a></li>
+                <li><a href="./?action=account"><i class="fa-solid fa-user"></i></a></li>
+            <?php } else { ?>
+                <li><a href="./?action=authentication"><i class="fa-solid fa-basket-shopping"></i>Panier / Nous trouver</a></li>
+                <li><a href="./?action=authentication"><i class="fa-solid fa-user"></i></a></li>
+            <?php } ?>
+        </ul>
+        <ul>
+            <li class="hideOnMobile"><a href="./?action=default">Accueil</a></li>
+            <li class="hideOnMobile"><a href="./?action=products">Nos bières</a></li>
+            <?php if (isset($_SESSION['mail'])) { ?>
+                <li class="hideOnMobile"><a href="./?action=cart"><i class="fa-solid fa-basket-shopping"></i> Panier / Nous trouver</a></li>
+                <li class="hideOnMobile"><a href="./?action=account"><i class="fa-solid fa-user"></i></a></li>
+            <?php } else { ?>
+                <li class="hideOnMobile"><a href="./?action=authentication"><i class="fa-solid fa-basket-shopping"></i>Panier / Nous trouver</a></li>
+                <li class="hideOnMobile"><a href="./?action=authentication"><i class="fa-solid fa-user"></i></a></li>
+            <?php } ?>
+            <li onclick=showSidebar()><i id="navBurger" class=" menuButton fa-solid fa-bars"></i></li>
 
-        <hr>
+        </ul>
+    </nav>
+
+    <hr>
     <main>
