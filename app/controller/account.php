8a8409ab8fa46467569a73e2555e45a92e42e603
MarionDuFaouet <lozachm@gmail.com>
2024-03-31 22:42:15 +0200

    fonctionne pas

--- app/controller/account.php
+++ app/controller/account.php
@@ -1,8 +1,18 @@
 <?php
-require_once RACINE . "/views/viewAccount.php";
-?>
 
-<!-- // // si je veux interdire l'accès à une page à l'admin
-// if (!((isset($_SESSION['admin'])) && $_SESSION['admin'])) {
-//     $_SESSION['msg']=['level'=> 'warning', 'content' => 'You must be an administrator to reach this page'];
-// } -->
+require_once RACINE . "/model/db.product.php";
+require_once RACINE . "/model/db.cart.php";
+
+require_once RACINE . "/model/db.user.php";
+// obtenir l'identifiant de compte de l'utilisateur authentifié, 
+// rangé dans la session ($accountId)
+
+
+
+// Obtenir la liste des commandes
+$orderProducts = getCartByAccountId($accountId);
+
+// Inclure la vue du compte client
+require_once RACINE . "/views/viewAccount.php";
+exit;
+?>
