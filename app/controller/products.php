e2d661fa9a0a9f7512925882db320e2dbec8812f
MarionDuFaouet <lozachm@gmail.com>
2024-03-31 22:46:15 +0200

    fonctionne pas

--- app/controller/products.php
+++ app/controller/products.php
@@ -1,29 +1,62 @@
 <?php
 
-require_once RACINE . "/model/db.product.php";
 
+// ma messagerie ne fonctionne pas !!!
+$msg = null;
+
+require_once RACINE . "/model/db.product.php";
+// require_once RACINE . "/model/db.cart.php";
+
+// Affiche les produits
 $products = getProducts();
 
-require_once RACINE . "/views/viewProducts.php";
 
 
-function addToCart($productId) {
+// Vérifie si le formulaire a été soumis et si l'action est d'ajouter au panier
+if (isset($_POST['addToCart'])) {
+    // Vérifie si productId est défini dans les données postées
+    if (isset($_POST['productId'])) {
+        // Récupère l'identifiant du produit depuis les données postées
+        $productId = $_POST['productId'];
+        // Appele la fonction addToCart avec l'identifiant du produit
+        addToCart($productId);
+        var_dump($productId);
+    }
+    
+}
 
-    // Vérifiez si le panier est déjà créé dans la session, sinon, créez-le
-    if (!isset($_SESSION["cart"])) {
-        $_SESSION["cart"] = array();
+
+
+
+
+function addToCart($productId)
+{
+    $msg = null;
+
+    // Initialiser la session si elle n'est pas déjà démarrée
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
     }
 
-    // Ajoutez le produit au panier
-    $_SESSION["cart"]["productId"] = $productId;
+    // Vérifier si le produit est déjà présent dans le panier
+    if (isset($_SESSION["cart"][$productId])) {
+        // Si oui, incrémente simplement la quantité
+        $_SESSION["cart"][$productId]++;
+        $msg = 'Produit ajouté avec succès !';
 
-    // Afficher un message de succès dans une pop-up
-    echo "<script>alert('Produit ajouté avec succès !');</script>";
+    } else {
+        // Si le produit n'est pas déjà dans le panier, ajoutez-le
+        $_SESSION["cart"][$productId] = 1;
+        $msg = 'Produit ajouté avec succès !';
+
+    }
+
+    // Afficher les informations du panier après l'ajout du produit
+    var_dump($_SESSION["cart"]);
 }
+
+// Inclure la vue des produits
+require_once RACINE . "/views/viewProducts.php";
+exit;
+
 ?>
-<!-- // // si je veux interdire l'accès à une page à l'admin
-// if (!((isset($_SESSION['admin'])) && $_SESSION['admin'])) {
-//     $_SESSION['msg']=['level'=> 'warning', 'content' => 'You must be an administrator to reach this page'];
-// } -->
-
-
