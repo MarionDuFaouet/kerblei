29ce93318bd9e93a74c817c2e9062048ad0af9ca
MarionDuFaouet <lozachm@gmail.com>
2024-03-31 23:18:53 +0200

    ajout msg et image

--- app/controller/cart.php
+++ app/controller/cart.php
@@ -2,34 +2,21 @@
 
 $msg = null;
 
-function addToCart($productId) {
+// Il faudrait que quand on arrive sur cette page et que le panier est vide, 
+$msg = 'Votre panier est vide';
 
-    // Vérifiez si le panier est déjà créé dans la session, sinon, créez-le
-    if (!isset($_SESSION["cart"])) {
-        $_SESSION["cart"] = array();
-    }
-    // Ajoutez le produit au panier
-    $_SESSION["cart"]["productId"] = $productId;
-    // Afficher un message de succès
-    $msg = 'Produit ajouté avec succès !';
+//sinon, affichage panier
 
-    echo "<script>alert('Produit ajouté avec succès !');</script>";
-}
 
-/* successfull add product to cart */
-$_SESSION['cart'] = $user['cart'];
-//$_SESSION['firstname'] = $user['firstname'];
-$_SESSION['admin'] = $user['isAdmin'];
-$msg = 'Salut ' . $user['firstname'] . '! Vous êtes connecté.';
 
 // ##DEBUG
-var_dump($_SESSION);
+// var_dump($_SESSION['cart']);
 
 
 require_once RACINE . "/views/viewCart.php";
-?>
 
-<!-- // // si je veux interdire l'accès à une page à l'admin
+
+// // si je veux interdire l'accès à une page à l'admin
 // if (!((isset($_SESSION['admin'])) && $_SESSION['admin'])) {
 //     $_SESSION['msg']=['level'=> 'warning', 'content' => 'You must be an administrator to reach this page'];
-// } -->
+// }
