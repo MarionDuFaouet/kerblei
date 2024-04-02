0068d63f23450313c700f8680f99d4e5dbc4d42c
MarionDuFaouet <lozachm@gmail.com>
2024-04-03 00:24:53 +0200

    retrait dim map

--- app/views/viewMap.php
+++ app/views/viewMap.php
@@ -3,7 +3,12 @@
         <h2>La Brasserie</h2>
         <p><adress>5 rue de kernavalo <br> 56870 BADEN</adress></p>
         <p>Retrouvez-nous sur le marché de Baden <br> tous les samedis matin</p>
+        <p>01 01 01 01 01</p>
     <img src="./statics/images/marché.jpg" alt="stand marché Kerblei">
     </div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2689.370957108478!2d-2.8886619240440456!3d47.618919971191005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4810108fd9ea692d%3A0x59435d2a947e4993!2s5%20Rue%20de%20Kernavalo%20Man%C3%A9%20Bihan%2C%2056870%20Baden!5e0!3m2!1sfr!2sfr!4v1711180841533!5m2!1sfr!2sfr"  style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2689.370957108478!2d-2.
+        8886619240440456!3d47.618919971191005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!
+        1s0x4810108fd9ea692d%3A0x59435d2a947e4993!2s5%20Rue%20de%20Kernavalo%20Man%C3%A9%20Bihan%2
+        C%2056870%20Baden!5e0!3m2!1sfr!2sfr!4v1711180841533!5m2!1sfr!2sfr"  style="border:0;" 
+        allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </aside>
