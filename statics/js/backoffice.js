99510a3146459715754f169343407a8b357cabbd
MarionDuFaouet <lozachm@gmail.com>
2024-04-02 11:42:01 +0200

    js déplacé

--- statics/js/backoffice.js
+++ statics/js/backoffice.js
@@ -43,3 +43,15 @@ function arrowNavigation(e){
         tabs[tabFocus].focus()
     }
 }
+
+// Fonction pour remplir le formulaire avec les données du produit sélectionné
+function fillForm(productId) {
+    // Remplir les champs du formulaire avec les données du produit sélectionné
+    document.getElementById("selectedProductId").value = productId;
+    document.getElementById("productName").value = "<?php echo $product['name']; ?>";
+    document.getElementById("productDegre").value = "<?php echo $product['degree']; ?>";
+    document.getElementById("productDescription").value = "<?php echo $product['designation']; ?>";
+    document.getElementById("productPrice").value = "<?php echo $product['unitPrice']; ?>";
+    document.getElementById("productPictureRef").value = "<?php echo $product['pictureRef']; ?>";
+}
+
