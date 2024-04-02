0c750fdfc3d8e627c4cffcaa05d225362dc70fc1
MarionDuFaouet <lozachm@gmail.com>
2024-04-02 16:00:52 +0200

    correction contrainte mot de passe, tel

--- app/controller/register.php
+++ app/controller/register.php
@@ -6,24 +6,21 @@ require RACINE . "/model/db.user.php";
 $registered = false;
 $msg = null;
 
-if (isset($_POST["mail"]) && isset($_POST["password"]) && isset($_POST["name"])&& isset($_POST["firstname"])&& isset($_POST["phone"])) {
+if (isset($_POST["mail"]) && isset($_POST["password"]) && isset($_POST["name"])&& isset($_POST["firstname"])) {
  
-    if ($_POST["mail"] != "" && $_POST["password"] != "" && $_POST["name"] != "" && $_POST["firstname"] != "" && $_POST["phone"] != "") {
+    if ($_POST["mail"] != "" && $_POST["password"] != "" && $_POST["name"] != "" && $_POST["firstname"] != "") {
         $mail = $_POST["mail"];
         $password = $_POST["password"];
         $name = $_POST["name"];
         $firstname = $_POST["firstname"];
-        $phone = $_POST["phone"];
 
         // Vérification de la longueur du mot de passe et des caractères précis (au moins une majuscule et un caractère spécial)
-        if (strlen($password) < 8 || strlen($password) > 12) {
-            $msg = "Le mot de passe doit contenir entre 8 et 12 caractères.";
-        } else if (!preg_match("/[A-Z]/", $password) || !preg_match("/[!@#$%^&*()\-_=+{};:,<.>]/", $password)) {
-            $msg = "Le mot de passe doit contenir au moins une lettre majuscule et un caractère spécial.";
+        if (strlen($password) < 8 || !preg_match("/[!@#$%^&*()\-_=+{};:,<.>]/", $password)) {
+            $msg = "Le mot de passe doit contenir au moins 8 caractères dont 1 caractère spécial.";
         } else {
             if (getUserByMail($mail) != null) $msg = "L'utilisateur existe déjà.";
             else{
-                // Enregistrement des données
+                // register user data
                 $ret = addUser($mail, $password, $name, $firstname, $phone);
                 if ($ret) {
                     $registered = true;
--- app/views/viewRegister.php
+++ app/views/viewRegister.php
@@ -7,20 +7,13 @@ $title = "Kerblei - S'inscrire";
 // include header
 require_once RACINE . "/views/header.php";
 ?>
-
-<!-- question : puis-je ajouter ce head? (pour ne pas indexer cette page) -->
-<head>
-    <meta name="robots" content="noindex" />
-    <meta name="googlebot" content="noindex" />
-</head>
 <!-- --------------------------------------------------------- -->
 
 
 <h2>Je créé mon compte</h2>
-
 <p class="msg"><?php echo $msg; ?></p>
 
-<form action="./?action=register" method="POST">
+<form class="forms" action="./?action=register" method="POST">
 
     <label for="name">Mon nom</label>
     <input type="text" name="name" placeholder="Nom" /><br />
@@ -31,11 +24,8 @@ require_once RACINE . "/views/header.php";
     <label for="mail">J'entre mon identifiant</label>
     <input type="email" name="mail" placeholder="Mon Email" /><br />
 
-    <label for="phone">J'entre mon numéro de téléphone</label>
-    <input type="phone" name="phone" placeholder="Mon numéro de téléphone" /><br />
-
     <label for="password">J'entre mon mot de passe</label>
-    <input type="password" name="password" placeholder="8 à 12 caratères, 1 majuscule et un caractère spécial"  /><br />
+    <input type="password" name="password" placeholder="au moins 8 caratères, dont caractère spécial"  /><br />
 
     <input class="cta-button" type="submit" title="Création votre compte" value="Je créé mon compte"/>
 
