818a5bb5176852bd027517f2a4ad20feb6a9a488
MarionDuFaouet <lozachm@gmail.com>
2024-04-03 10:14:19 +0200

    ajout message header

--- app/views/viewProducts.php
+++ app/views/viewProducts.php
@@ -4,13 +4,20 @@
 $description = "Découvrez notre gamme de bières artisanales.";
 $title = "Kerblei - Nos produits";
 
-
-
 require_once RACINE . "/views/header.php";
 ?>
+<!-- -------------------------------------------------------------------------- -->
+
 <h2>Nos produits</h2>
-<!-- show message -->
-<p class="msg"><?php echo $msg; ?></p>
+
+<header class="container">
+    <p>Chers clients. Ici vous pouvez réserver vos bières par simple clic, nous
+        préparons votre commmande et vous la livrons le jour de votre choix au marché
+        de Baden. Paiement sur place à la récupération de votre commande.</p>
+    <!-- show message -->
+    <p class="msg"><?php echo $msg; ?></p>
+</header>
+
 <!-- show products -->
 <section class="container product">
     <?php foreach ($products as $product) : ?>
