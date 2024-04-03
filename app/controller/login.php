f222a722bfc436da658865eb0815a288fb24e838
MarionDuFaouet <lozachm@gmail.com>
2024-04-03 10:09:16 +0200

    correction redirection à la connexion

--- app/controller/login.php
+++ app/controller/login.php
@@ -1,21 +1,22 @@
 <?php
 
 $msg = null;
-$products=[];
+// $products=[];
 
 
-// user already logged
-if (isset($_SESSION['mail'])) {
-    $msg = 'Vous êtes déjà connecté au compte associé à ce mail "' . $_SESSION['mail'] . '". Merci de vous déconnecter.';
-    require RACINE .  "/views/viewLogin.php";
-    exit;  
-}
+// // user already logged
+// if (isset($_SESSION['mail'])) {
+//     $msg = 'Vous êtes déjà connecté au compte associé à ce mail "' . $_SESSION['mail'] . '". Merci de vous déconnecter.';
+//     require RACINE .  "/views/viewLogin.php";
+//     exit;  
+// }
 
 //  if not already logged in
 if (!isset($_POST['mail']) || !isset($_POST['password'])) {
     require RACINE .  "/views/viewLogin.php";
     exit;
 }
+
 
 /* empty inputs? */
 if (empty($_POST['mail']) || empty($_POST['password'])) {
@@ -24,29 +25,46 @@ if (empty($_POST['mail']) || empty($_POST['password'])) {
     exit;
 }
 
+
 /* Now, submit login form */
 require RACINE . "/model/db.user.php";
 // login($mail, $password);
 $user = getUserByMail($_POST['mail']);
+
 if (($user == null) || ($_POST['password'] != $user['password'])) {
     $msg = 'Identifiant ou mot de passe erroné';
     require RACINE . "/views/viewLogin.php";
     exit;
 }
 
+
 /* successfull login */
 $_SESSION['mail'] = $user['mail'];
 $_SESSION['admin'] = $user['isAdmin'];
 
-
 // ##DEBUG
 // var_dump($_SESSION);
 
-if ($_SESSION['admin']==0){
-    require RACINE . "/views/viewAccount.php";
-// logged Go to home page
-} else require RACINE . "/views/viewAdmin.php"; 
-// } else require RACINE . "admin.php"; 
+// Redirection en fonction du type d'utilisateur (client ou administrateur)
+if ($_SESSION['admin'] == 0) {
+    header("Location: ./?action=account");
+} else {
+    header("Location: ./?action=admin");
+}
+
+exit;
+?>
 
 
-// pas au point mon affaire, ça m'envoie encore vers login
+
+
+
+
+
+
+
+
+
+
+
+
