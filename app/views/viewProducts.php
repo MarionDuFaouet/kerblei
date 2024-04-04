e2a408040764e612f98d6485fd909270e705a4e4
MarionDuFaouet <lozachm@gmail.com>
2024-04-04 11:39:39 +0200

    mise en panier ok

--- app/views/viewProducts.php
+++ app/views/viewProducts.php
@@ -14,11 +14,13 @@ require_once RACINE . "/views/header.php";
     <p>Chers clients, ici vous pouvez réserver vos bières en quelques clic. <br>
         Nous préparons votre commmande et vous la livrons le samedi de votre choix au marché de Baden.</p>
     <p class="msg">Paiement sur place à la récupération de votre commande.</p>
+    <p class="msg"><?php echo $msg; ?></p>
 
 </header>
 
 <!-- show products -->
 <section class="container product">
+
     <?php foreach ($products as $product) : ?>
         <div class="productShow">
 
@@ -30,11 +32,8 @@ require_once RACINE . "/views/header.php";
                 <p><?php echo $product['designation']; ?></p>
                 <p><?php echo $product['unitPrice']; ?> &#x20AC</p>
                 <!-- Champ de formulaire caché pour productId -->
-                <!-- <input type="hidden" name="productId" value="<?php echo $product['productId']; ?>"> -->
-                <!-- ###DEBUG -->
-                <p><?php echo $product['productId']; ?></p>
-                <p class="msg"><?php echo $msg; ?></p>
-
+                <input type="hidden" name="productId" value="<?php echo $product['productId']; ?>">
+                
                 <!-- bouton panier -->
                 <button type="submit" class="cta-button" name="addToCart" value="<?php echo $product['productId']; ?>">Ajouter au panier</button>
             
