62e2907336b728e57ad2d781601735f65bc04af4
MarionDuFaouet <lozachm@gmail.com>
2024-04-05 11:15:56 +0200

    retrait required aux entrées du formulaire

--- app/views/viewAccount.php
+++ app/views/viewAccount.php
@@ -28,17 +28,17 @@ include RACINE . '/views/header.php' ?>
     <label for="name">Modifier mon nom</label>
     <input type="text" id="name" name="name" placeholder="mon nom" 
     value="<?php echo isset($_POST['name']) ? ($_POST['name']) : ($user['name'] ?? ''); ?>" 
-    required maxlength="20"><br />
+     maxlength="20"><br />
     
     <label for="firstname">Modifier mon prénom</label>
     <input type="text" id="firstname" name="firstname" placeholder="mon prénom" 
     value="<?php echo isset($_POST['firstname']) ? ($_POST['firstname']) : ($user['firstname'] ?? ''); ?>" 
-    required maxlength="20"><br />
+     maxlength="20"><br />
     
     <label for="phone">Modifier mon téléphone</label>
     <input type="tel" id="phone" name="phone" placeholder="01 01 01 01 01" 
     value="<?php echo isset($_POST['phone']) ? ($_POST['phone']) : ($user['phone'] ?? ''); ?>" 
-    required pattern="[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}"><br />
+     pattern="[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}"><br />
     
     <label for="password">Modifier mon mot de passe</label>
     <input type="password" id="password" name="password" placeholder="Mot de passe" 
