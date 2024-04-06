0cfff9aaf7db7af48fca0231ed80da5612b90ca1
MarionDuFaouet <lozachm@gmail.com>
2024-04-06 19:32:02 +0200

    chgt carte, openstreetmap, pb étiquettes

--- app/views/viewMap.php
+++ app/views/viewMap.php
@@ -6,9 +6,12 @@
         <p>01 01 01 01 01</p>
     <img src="./statics/images/marché.jpg" alt="stand marché Kerblei">
     </div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2689.370957108478!2d-2.
-        8886619240440456!3d47.618919971191005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!
-        1s0x4810108fd9ea692d%3A0x59435d2a947e4993!2s5%20Rue%20de%20Kernavalo%20Man%C3%A9%20Bihan%2
-        C%2056870%20Baden!5e0!3m2!1sfr!2sfr!4v1711180841533!5m2!1sfr!2sfr"  style="border:0;" 
-        allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    
+
+<iframe width="100%" height="300px" frameborder="0" allowfullscreen allow="geolocation" 
+src="//umap.openstreetmap.fr/fr/map/new/?scaleControl=false&miniMap=false&scrollWheelZo
+om=false&zoomControl=null&editMode=disabled&moreControl=true&searchControl=null&tilelay
+ersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=fa
+lse&captionMenus=false&fullscreenControl=null&datalayers=5c0eb808-3cff-443b-8f33-b4f69b
+25175a"></iframe>
 </aside>
