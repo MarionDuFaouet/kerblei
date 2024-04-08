e8f064b769f860da2a4cf306bcd848139c15791a
MarionDuFaouet <lozachm@gmail.com>
2024-04-08 17:47:19 +0200

    map ok

--- app/views/viewMap.php
+++ app/views/viewMap.php
@@ -7,11 +7,7 @@
     <img src="./statics/images/marché.jpg" alt="stand marché Kerblei">
     </div>
     
+    <div id="myMap"></div>
 
-<iframe width="100%" height="300px" frameborder="0" allowfullscreen allow="geolocation" 
-src="//umap.openstreetmap.fr/fr/map/new/?scaleControl=false&miniMap=false&scrollWheelZo
-om=false&zoomControl=null&editMode=disabled&moreControl=true&searchControl=null&tilelay
-ersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=fa
-lse&captionMenus=false&fullscreenControl=null&datalayers=5c0eb808-3cff-443b-8f33-b4f69b
-25175a"></iframe>
 </aside>
+
---
+++ statics/js/map.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+        //console.log("DOM fully loaded and parsed");
+
+        const map = L.map('myMap').setView([47.62018, -2.88567], 12);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([47.62018, -2.88567]).addTo(map)
+            .bindPopup('Brasserie Kerblei<br>5 rue de kernavalo<br>56870 BADEN')
+            .openPopup();
+    });
