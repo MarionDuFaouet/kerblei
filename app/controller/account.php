7e30589a19f9adf95d3e684fd9d6d39da6c36e4f
MarionDuFaouet <lozachm@gmail.com>
2024-04-11 20:15:54 +0200

    account ok

--- app/controller/account.php
+++ app/controller/account.php
@@ -3,33 +3,19 @@
 require_once RACINE . "/model/db.user.php";
 $msg = null;
 
-
-###DEBUG
-// Récupérer les données de l'utilisateur depuis la base de données
-// var_dump($mail);
-
 $mail = $_SESSION['mail'];
 $user = getUserByMail($mail);
 $accountId= $user['accountId'];
+// -----------------------------------------------------------------------------
 
-
-
-// liste des commandes
+// get Orders
 require_once RACINE . "/model/db.cart.php";
-
 $orders = getOrdersByAccountId($accountId);
 
-
-
-
-
-
-
-// formulaire modif données perso
+// fill form with account datas
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (empty($_POST["name"]) && empty($_POST["firstname"]) && empty($_POST["phone"]) && empty($_POST["password"])) {
         if ($user) {
-            // Préremplir le formulaire avec les données de l'utilisateur
             $_POST["name"] = $user['name'] ?? '';
             $_POST["firstname"] = $user['firstname'] ?? '';
             $_POST["phone"] = $user['phone'] ?? '';
@@ -38,32 +24,24 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 }
 
-
-// Modifications des données
+// Update datas
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["updateUser"])) {
     if (isset($_POST["name"]) || isset($_POST["firstname"]) || isset($_POST["phone"]) || isset($_POST["password"])) {
-        // Retrieve form data
         $name = $_POST["name"];
         $firstname = $_POST["firstname"];
         $phone = $_POST["phone"];
         $passwordHash = $_POST["password"];
 
-        // Call the model function to update user's data
         updateUser($mail, $name, $firstname, $phone, $passwordHash);
         var_dump(updateUser($mail, $name, $firstname, $phone, $passwordHash));
     }
-    // Suppression des données
+    // delete datas
 } elseif ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["deleteUser"])) {
-    // faire apparaître un message pour être informer de l'irreversibilité de l'action
     deleteUser($mail);
-    // faire apparaître ce message sur home, pendant quelques secondes
     $msg = 'Compte supprimé';
     exit;
 }
 
-
-
-
-// Inclure la vue du compte client
+// ---------------------------------------------------------------------------------
 require_once RACINE . "/views/viewAccount.php";
 exit;
--- app/views/viewAccount.php
+++ app/views/viewAccount.php
@@ -1,5 +1,4 @@
 <?php
-// meta description and title
 $title = "Kerblei - Mon compte";
 $description = "Mon compte Kerblei";
 
@@ -7,20 +6,15 @@ require_once RACINE . '/views/head.start.php';
 require_once RACINE . '/views/head.stop.php';
 require_once RACINE . '/views/menu.php';
 ?>
-
 <!-- --------------------------------------------------------------------------- -->
 <h2>Bienvenue <?php echo ($user['firstname']); ?></h2>
-
-
 
 <h2>Mes commandes</h2>
 <section class="container">
 
-
     <table class="accountTable">
         <thead>
             <tr>
-                <th scope="col">Date de commande</th>
                 <th scope="col">Contenu</th>
                 <th scope="col">Prix total</th>
                 <th scope="col">Date de retrait</th>
@@ -30,9 +24,8 @@ require_once RACINE . '/views/menu.php';
         <tbody>
             <?php foreach ($orders as $order) : ?>
                 <tr>
-                    <td><?php echo $order['orderDate']; ?></td>
                     <td><?php echo $order['productName']; ?></td>
-                    <td><?php echo $order['productUnitPrice'] * $order['productQuantity'].'&euro'; ?></td>
+                    <td><?php echo $order['productUnitPrice'] * $order['productQuantity'].' &#8364;'; ?></td>
                     <td><?php echo $order['deliveryDate']; ?></td>
                     <td><?php echo $order['statement']; ?></td>
                 </tr>
@@ -40,35 +33,24 @@ require_once RACINE . '/views/menu.php';
         </tbody>
     </table>
 
-
-    
-
 </section>
-
 
 <hr>
 <!-- --------------------------------------------------------------------------- -->
 <h2>Modifier mes données personnelles</h2>
 
-
 <p class="msg"><?php echo $msg; ?></p>
-
 
 <form class="forms" action="./?action=account" method="POST">
 
-
     <label for="name">Modifier mon nom</label>
     <input type="text" id="name" name="name" placeholder="mon nom" value="<?php echo isset($_POST['name']) ? ($_POST['name']) : ($user['name'] ?? ''); ?>" maxlength="20"><br />
-
     <label for="firstname">Modifier mon prénom</label>
     <input type="text" id="firstname" name="firstname" placeholder="mon prénom" value="<?php echo isset($_POST['firstname']) ? ($_POST['firstname']) : ($user['firstname'] ?? ''); ?>" maxlength="20"><br />
-
     <label for="phone">Modifier mon téléphone</label>
     <input type="tel" id="phone" name="phone" placeholder="01 01 01 01 01" value="<?php echo isset($_POST['phone']) ? ($_POST['phone']) : ($user['phone'] ?? ''); ?>" pattern="[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}"><br />
-
     <label for="password">Modifier mon mot de passe</label>
     <input type="password" id="password" name="password" placeholder="Mot de passe" minlength="8" pattern="(?=.*[!@#$%^&*()\-_=+{};:,<.>])\S+"><br />
-
     <!-- to modify or delete account -->
     <div class="accountAction">
         <input class="cta-button" name="updateUser" type="submit" title="Modification de vos données" value="Je valide ces modifications" />
@@ -78,7 +60,5 @@ require_once RACINE . '/views/menu.php';
     <a href="./?action=logout" class="cta-button" title="Cliquez ici pour vous déconnecter">Se déconnecter</a>
 
 </form>
-
-
 
 <?php include RACINE . "/views/footer.php"; ?>
