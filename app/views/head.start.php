18b4469d9c024a100e0ad9fe642483da60413b0b
MarionDuFaouet <lozachm@gmail.com>
2024-04-11 20:26:20 +0200

    head ok

--- app/views/head.start.php
+++ app/views/head.start.php
@@ -6,11 +6,8 @@
     <meta charset="UTF-8" />
     <!--noindex-->
     <?php
-    // verify actual page
     $currentPage = basename($_SERVER['PHP_SELF']);
-    // list pages where no index must be
     $noIndexPages = array('login.php', 'logout.php', 'cart.php', 'account.php', 'admin.php', 'register.php', 'legalNotice.php');
-    // verifiy if actual page is in this list
     if (in_array($currentPage, $noIndexPages)) {
         echo '<meta name="robots" content="noindex" />';
         echo '<meta name="googlebot" content="noindex" />';
@@ -19,14 +16,18 @@
     <meta name="description" content="<?php echo $description; ?>" />
     <meta name="author" content="Marion Lozach" />
     <title><?php echo $title; ?></title>
-    <!-- CSS/JS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
-    <link rel="stylesheet" href="./statics/styles/style.css">
-    <!-- <link rel="stylesheet" href="./statics/styles/cart.css"> -->
-
+    <!-- CSS -->
+    <link rel="stylesheet" 
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" 
+          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" 
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" 
+          crossorigin="" />
+    <link rel="stylesheet" href="./statics/styles/style.min.css">
+    <!-- JS -->
     <script src="./statics/js/headerScript.js" defer></script>
-    <!-- <script src="./statics/js/account.js" defer></script> -->
     <script src="./statics/js/cart.js" defer></script>
 
     
