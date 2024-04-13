3120a8154613c8012aceca482e69d684b44c61fc
MarionDuFaouet <lozachm@gmail.com>
2024-04-13 18:38:07 +0200

    styles ok

--- statics/js/cart.js
+++ statics/js/cart.js
@@ -1,31 +1,11 @@
-// Ce code JavaScript est une fonction nommée addProductInCart qui est utilisée 
-// pour ajouter un produit au panier dans une application web. Voici une explication ligne par ligne :
-// La fonction addProductInCart prend en paramètre l'ID du produit à ajouter au panier.
-// La fonction utilise l'API fetch pour effectuer une requête HTTP GET vers une URL. 
-// Cette URL est construite avec le paramètre action égal à "cartAdd" et l'ID du produit fourni.
-// Les options de la requête fetch comprennent la méthode GET et un en-tête spécifiant que le contenu 
-// de la requête est au format JSON.
-// Ensuite, une série de promesses (Promise) est enchaînée à l'aide de .then :
-// Le premier .then vérifie si la réponse de la requête est OK (statut HTTP 200). Si c'est le cas,
-// il retourne le contenu JSON de la réponse.
-// Le deuxième .then traite le contenu JSON de la réponse, généralement mis à jour en fonction de 
-// l'action de l'utilisateur. Dans ce cas, il met à jour le panier sur l'interface utilisateur en 
-// manipulant le DOM, puis met à jour un badge affichant le nombre d'articles dans le panier.
-// Si une erreur se produit à n'importe quelle étape de la chaîne de promesses, elle est gérée par 
-// le bloc .catch, qui affiche simplement le message d'erreur dans la console.
-
-// En résumé, cette fonction envoie une requête pour ajouter un produit au panier, met à jour 
-// l'interface utilisateur en fonction de la réponse serveur et gère les erreurs qui pourraient 
-// survenir lors du processus.
-
 /**
- * Ajoute un produit au panier.
- * @param {string} productId - L'identifiant du produit à ajouter au panier.
+ * Add product to cart
+ * @param {string} productId 
  */
 function addProductInCart(productId) {
-    fetch('?action=cartAdd&productId=' + productId,    // URL
+    fetch('?action=cartAdd&productId=' + productId,
         {
-            method: 'GET', // GET, POST, PUT, DELETE, etc.
+            method: 'GET',
             headers: {
                 "Content-Type": "application/json"
             }
@@ -98,6 +78,42 @@ function updateProductInCart(productId) {
 
 
 
+
+
+
+
+
+
+
+// Badge
+function updateCartBadge(count) {
+    const badge = document.querySelector('#cartBadge');
+    badge.innerHTML = count;
+}
+
+/*------ Cart display -------*/
+function showCart(event) {
+    cartModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function hideCart(event) {
+    cartModal.style.display = "none";
+}
+
+
+// ouverture et fermeture du panier
+function addAllListeners(event) {
+    const cartButton = document.querySelector("#cartButton");
+    if (cartButton) cartButton.addEventListener("click", showCart);
+
+    // it could be shared among several modal contents
+    const close = document.querySelector(".close");
+    if (close) close.addEventListener("click", hideCart);
+
+}
+
+document.addEventListener("DOMContentLoaded", addAllListeners);
 
 
 
